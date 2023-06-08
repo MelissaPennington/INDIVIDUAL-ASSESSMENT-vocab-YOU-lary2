042860110246877990120e2621b28c07f410ead1
melissapennington <mlpennington9@gmail.com>
2023-06-08 07:21:06 -0500

    some parts

---
+++ api/entryData.js
@@ -0,0 +1,45 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+const getEntries = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/entries.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const createEntry = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/entries.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateEntry = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/entries/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getEntries, createEntry, updateEntry
+};
